0fb1216cca5b9516e53f0d78db2ae0a1394de659
shujie <shujie@gitee.com>
2022-10-16 08:29:11 -0700

    grep zip file from github

---
+++ project_info.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""
+源文件名：project_info.py
+功能描述：
+作者：ShuJie
+版本：V0.1
+创建时间：2022/10/16 15:24
+
+修改历史：
+修改时间：
+版本号：
+修改人：
+修改内容：
+"""
+# 库引入
+
+# 变量声明
+PROJECT_AUTHOR = 'Omity'
+PROJECT_NAME = 'py_tools'
+
+# 函数定义
+# def 
+# 类定义
+
