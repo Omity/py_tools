d8f60f79fc8a4995d4ba7d51769a3a65458d1786
shujie <shujie@gitee.com>
2022-11-08 23:01:35 +0800

    add serial check functions

--- main.py
+++ main.py
@@ -18,11 +18,13 @@
 import sys
 import ctypes
 
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QPropertyAnimation, QPoint
+from PyQt5.QtGui import QIcon, QCursor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QButtonGroup, QWidget
 from pytools import Ui_pyTools
 from asciiTool import AsciiTool
 from conversionTool import ConversionTool
+from serialTool import SerialTool
 
 # 变量声明
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('PY TOOLS')
@@ -39,16 +41,21 @@ class PyTools(QWidget, Ui_pyTools):
         self.setWindowTitle('PY TOOLS')
         self.setWindowIcon(QIcon('py_tool.ico'))
 
+        self.animation = QPropertyAnimation(self, b"pos", self)
+
         self.ascii = AsciiTool(self)
         self.conversion = ConversionTool(self)
+        self.serial = SerialTool(self)
 
         self.buttonItem = [
             self.ascii,
-            self.conversion
+            self.conversion,
+            self.serial
         ]
 
         self.stackedWidget.addWidget(self.ascii)
         self.stackedWidget.addWidget(self.conversion)
+        self.stackedWidget.addWidget(self.serial)
 
         self.buttonGroup = QButtonGroup(self)
         self.buttonAdd()
@@ -61,9 +68,26 @@ class PyTools(QWidget, Ui_pyTools):
     def buttonAdd(self):
         self.buttonGroup.addButton(self.asciiBtn, id=0)
         self.buttonGroup.addButton(self.conversionBtn, id=1)
+        self.buttonGroup.addButton(self.serialBtn, id=2)
 
     def buttonChanged(self, index):
         self.stackedWidget.setCurrentWidget(self.buttonItem[index])
+
+    def enterEvent(self, a0):
+        if -10 - self.frameGeometry().height() <= self.y() <= -self.frameGeometry().height() + 10:
+            animation = QPropertyAnimation(self, b"pos", self)
+            animation.setStartValue(QPoint(self.x(), self.y()))
+            animation.setEndValue(QPoint(self.x(), 0))
+            animation.setDuration(200)
+            animation.start()
+
+    def leaveEvent(self, a0):
+        if self.y() <= 2 and QCursor.pos().y() > self.frameGeometry().height() - self.geometry().height():
+            animation = QPropertyAnimation(self, b"pos", self)
+            animation.setStartValue(QPoint(self.x(), self.y()))
+            animation.setEndValue(QPoint(self.x(),  2 - self.frameGeometry().height()))
+            animation.setDuration(200)
+            animation.start()
 
 
 if __name__ == '__main__':
