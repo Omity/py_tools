73a55b7094d10a6341bea78ec1fc64b52d94b680
shujie <shujie@1499570221.com>
2023-03-16 22:50:53 +0800

    * add pack and pack installer file

---
+++ pack.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""
+源文件名：pack.py
+功能描述：
+作者：ShuJie
+版本：V0.1
+创建时间：2022/10/4 19:17
+
+修改历史：
+修改时间：
+版本号：
+修改人：
+修改内容：
+"""
+# 库引入
+import os
+
+# 变量声明
+# G_para = '' 
+# 函数定义
+# def 
+# 类定义
+
+pyinstaller = r'venv\Scripts\pyinstaller.exe '
+mainFile = 'main.py'
+sourceFile = ['ascii.py', 'asciiTool.py', 'conversion.py', 'conversionTool.py', 'serial_tool.py', 'serialTool.py',
+              'pytools.py']
+resource = ['ascii_info.html;.']
+icoPic = 'py_tool.ico'
+noConsole = ' --noconsole '
+outPath = ' --distpath '
+tempPath = ' --workpath '
+specPath = ' -o '
+data = ' --add-data '
+packName = 'py_tools'
+
+
+def scanSrc(directory):
+    a_list = []
+    files = os.listdir(directory)
+    for filename in files:
+        path = os.path.join(directory, filename)
+        if os.path.isdir(path):
+            a_list.extend(scanSrc(path))
+        if os.path.isfile(path):
+            if os.path.splitext(filename)[1] == '.py':
+                a_list.append(path)
+    return a_list
+
+
+def scanData(directory):
+    a_list = []
+    files = os.listdir(directory)
+    for filename in files:
+        path = os.path.join(directory, filename)
+        if os.path.isdir(path):
+            a_list.extend(scanSrc(path))
+        if os.path.isfile(path):
+            a_list.append(path + f';{directory}')
+    return a_list
+
+
+def scanIco(directory):
+    a_list = []
+    files = os.listdir(directory)
+    for filename in files:
+        path = os.path.join(directory, filename)
+        if os.path.isdir(path):
+            a_list.extend(scanSrc(path))
+        if os.path.isfile(path):
+            # 排除ico
+            if os.path.splitext(filename)[1] == '.ico':
+                a_list.append(path)
+    return a_list
+
+
+if __name__ == '__main__':
+
+    current = os.getcwd()
+
+    pyinstaller = os.path.join(current, pyinstaller)
+
+    ico = ' -i ' + scanIco('./data')[0]
+    cmd = pyinstaller + ' -y ' + mainFile
+    for i in scanSrc('./api'):
+        cmd += ' -p ' + i
+    for i in scanSrc('./ui'):
+        cmd += ' -p ' + i
+
+    for i in scanData('./data'):
+        cmd += data + i
+
+    cmd += ico
+    cmd += noConsole
+    # 打包后的目录名字, 注意yml文件对应打包的时候需要对应
+    cmd += '-n ' + packName
+
+    os.system(cmd)
---
+++ ui_to_py.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""
+源文件名：ui_to_py.py
+功能描述：
+作者：ShuJie
+版本：V0.1
+创建时间：2022/2/12 22:49
+
+修改历史：
+修改时间：
+版本号：
+修改人：
+修改内容：
+"""
+
+# 库引入
+import os
+import os.path
+
+# 变量声明
+import time
+
+root_directory = './ui'
+is_modify_time = 5 * 60
+# 函数定义
+
+# 类定义
+
+# 更新过
+
+
+def isModified(curr, file):
+    create = os.path.getctime(file)
+    modify = os.path.getmtime(file)
+    if curr - create < is_modify_time:
+        return True
+    elif curr - modify < is_modify_time:
+        return True
+
+    return False
+
+
+# 列出目录下的所有UI文件
+def listUiFile(directory):
+    # 避免需要转换的时间过长, 我们先把当前时间获取
+    curr = time.time()
+    a_list = []
+    files = os.listdir(directory)
+    for filename in files:
+        path = os.path.join(directory, filename)
+        if os.path.isdir(path):
+            a_list.extend(listUiFile(path))
+        if os.path.isfile(path):
+            # 对应的py文件不存在
+            tmp = os.path.splitext(filename)[0] + '.py'
+            if not os.path.exists(os.path.join(os.path.dirname(path), tmp)):
+                a_list.append(path)
+                continue
+            if isModified(curr, path):
+                if os.path.splitext(filename)[1] == '.ui':
+                    a_list.append(path)
+    return a_list
+
+
+# 把扩展名为.ui的文件改成扩展名为.py的文件
+def transPyFile(filename):
+    return os.path.splitext(filename)[0] + '.py'
+
+
+# 调用系统命令把UI文件转换成Python文件
+def runMain(directory):
+    a_list = listUiFile(directory)
+    print(a_list)
+    for uiFile in a_list:
+        pyFile = transPyFile(uiFile)
+        cmd = 'pyuic5 -o {pyfile} {uifile}'.format(pyfile=pyFile, uifile=uiFile)
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    runMain(root_directory)
